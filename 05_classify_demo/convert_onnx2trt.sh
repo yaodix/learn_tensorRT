3a33b83846afeed7882cc45af9027fa9bc2b2cef
yaodix <liuyao199111@163.com>
2022-08-07 21:54:51 +0800

    ADD: 增加部署模块化相关内容

--- 05_classify_demo/convert_onnx2trt.sh
+++ 05_classify_demo/convert_onnx2trt.sh
@@ -1,5 +1,5 @@
  # 转换模型倾向使用trtexec 而不是onnx2trt
  
  cd /home/yao/TensorRT-7.2.3.4/targets/x86_64-linux-gnu/bin/
-./trtexec --onnx=/home/yao/workspace/learn_tensorRT/05_classify_demo/workspace/classifier.onnx 
-    --saveEngine=/home/yao/workspace/learn_tensorRT/05_classify_demo/workspace/classifier.trt
+./trtexec --onnx=/home/yao/workspace/learn_tensorRT/08_module_depoly/01_memory/workspace/classifier.onnx \
+    --saveEngine=/home/yao/workspace/learn_tensorRT/08_module_depoly/01_memory/workspace/classifier.trt
