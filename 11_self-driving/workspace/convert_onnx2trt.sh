d5dca6ad3bf5be3080ee1d635fe3aba1ce1dd8d8
yaodix <liuyao199111@163.com>
2023-08-02 22:46:34 +0800

    fix tensorrt

--- 11_self-driving/workspace/convert_onnx2trt.sh
+++ 11_self-driving/workspace/convert_onnx2trt.sh
@@ -1,5 +1,14 @@
  # 转换模型倾向使用trtexec 而不是onnx2trt
  
- cd /home/yao/TensorRT-7.2.3.4/targets/x86_64-linux-gnu/bin/
+ cd /opt/TensorRT-7.2.3.4/targets/x86_64-linux-gnu/bin/
 ./trtexec --onnx=/home/yao/workspace/learn_tensorRT/11_self-driving/workspace/ldrn_kitti_resnext101_pretrained_data_grad_256x512.onnx \
     --saveEngine=/home/yao/workspace/learn_tensorRT/11_self-driving/workspace/ldrn_kitti_resnext101_pretrained_data_grad_256x512.trt
+
+./trtexec --onnx=/home/yao/workspace/learn_tensorRT/11_self-driving/workspace/road-segmentation-adas.onnx \
+    --saveEngine=/home/yao/workspace/learn_tensorRT/11_self-driving/workspace/road-segmentation-adas.trt
+
+./trtexec --onnx=/home/yao/workspace/learn_tensorRT/11_self-driving/workspace/ultra_fast_lane_detection_culane_288x800.onnx \
+    --saveEngine=/home/yao/workspace/learn_tensorRT/11_self-driving/workspace/ultra_fast_lane_detection_culane_288x800.trt
+
+./trtexec --onnx=/home/yao/workspace/learn_tensorRT/11_self-driving/workspace/yolov5s.onnx \
+    --saveEngine=/home/yao/workspace/learn_tensorRT/11_self-driving/workspace/yolov5s.trt
